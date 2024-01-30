db9b2a2cc51e53345cf91876c2ceab9dc233afb2
sOrzada <95355338+sOrzada@users.noreply.github.com>
2024-01-30 09:43:33 +0100

    Update RunPostproc.m
    Code was missing a line to actually load the VOP-file.

--- PostProc/RunPostproc.m
+++ PostProc/RunPostproc.m
@@ -3,6 +3,9 @@
 
 filename_in='/home/ubuntu/Data/SARMatrix_6x4_matrices_tri.mat'; %Points to the file containing the full set of SAR matrices.
 name_VOP='VOP_Postproc_test_24ch_0p2'; %Points to a VOP-file (Contains VOPs and Sglobal).
+
+
+load(name_VOP);
 VOP_start=VOP;
 
 [VOP,Sglobal_new]=VOP_overestimation_postprocessing_hybrid(filename_in,VOP_start,Sglobal,name_VOP);
